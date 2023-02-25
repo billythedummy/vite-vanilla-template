be48688901b634a58bb51d64fa0916b2b86978ad
billythedummy <dhy1996@live.com.sg>
2023-02-26 01:34:29 +0800

    PWA

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 // silence `'vite' should be listed in project's dependencies, not devDependencies`
 
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // import path from "path" causes eslint to crash for some reason
 const path = require("path");
@@ -24,4 +25,37 @@ export default defineConfig({
       { find: "@", replacement: __dirname },
     ],
   },
+  plugins: [
+    VitePWA({
+      includeAssets: ["favicon.ico", "images/logo/apple-touch-icon.png"],
+      manifest: {
+        name: "My Web App",
+        short_name: "mwa",
+        description: "This is a test web app",
+        icons: [
+          {
+            src: "/images/logo/logo_512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/images/logo/logo_192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/logo/logo_192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        start_url: "index.html",
+        display: "fullscreen",
+        // TODO: USE ACTUAL THEME COLORS
+        theme_color: "#FFFFFF",
+        background_color: "#FFFFFF",
+      },
+    }),
+  ],
 });
