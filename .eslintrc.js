b7bb68b5138dcb377ef68c1bc338710fdca23410
billythedummy <dhy1996@live.com.sg>
2023-03-04 15:38:41 +0800

    add eslint-html

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,46 +4,57 @@ module.exports = {
     browser: true,
   },
   ignorePatterns: ["dist/*"],
-  plugins: ["simple-import-sort"],
-  extends: ["airbnb-base", "prettier"],
-  settings: {
-    "import/resolver": {
-      jsconfig: {
-        config: "jsconfig.json",
+  overrides: [
+    {
+      files: "*.html",
+      parser: "@html-eslint/parser",
+      plugins: ["@html-eslint"],
+      extends: ["plugin:@html-eslint/recommended"],
+    },
+    {
+      files: "*.js",
+      plugins: ["simple-import-sort"],
+      extends: ["airbnb-base", "prettier"],
+      parserOptions: {
+        ecmaVersion: "2020",
+        sourceType: "module",
+      },
+      settings: {
+        "import/resolver": {
+          jsconfig: {
+            config: "jsconfig.json",
+          },
+        },
+      },
+      rules: {
+        "import/prefer-default-export": "off",
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [".*"],
+          },
+        ],
+        "no-restricted-syntax": [
+          "error",
+          {
+            selector: "ForInStatement",
+            message:
+              "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
+          },
+          {
+            selector: "LabeledStatement",
+            message:
+              "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+          },
+          {
+            selector: "WithStatement",
+            message:
+              "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+          },
+        ],
+        "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+        "no-param-reassign": ["error", { props: false }],
       },
     },
-  },
-  parserOptions: {
-    ecmaVersion: "2020",
-    sourceType: "module",
-  },
-  rules: {
-    "import/prefer-default-export": "off",
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: [".*"],
-      },
-    ],
-    "no-restricted-syntax": [
-      "error",
-      {
-        selector: "ForInStatement",
-        message:
-          "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
-      },
-      {
-        selector: "LabeledStatement",
-        message:
-          "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
-      },
-      {
-        selector: "WithStatement",
-        message:
-          "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
-      },
-    ],
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    "no-param-reassign": ["error", { props: false }],
-  },
+  ],
 };
